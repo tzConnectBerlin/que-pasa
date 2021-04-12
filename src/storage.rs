6181dc9478c6aa9240641409246209cfc66dc147
John Newby <john@newby.org>
2021-04-12 11:39:27 +0200

    Whitespace more permissable now!

--- src/storage.rs
+++ src/storage.rs
@@ -21,11 +21,11 @@ peg::parser! {
         rule _() = [' ' | '\n']*
 
         pub rule address() -> Expr =
-            _ "(address " _ l:label() _ ")" { Expr::Address(Some(l.to_owned())) } /
+            _ "(address " _ l:label() _ ")" { Expr::Address(Some(l)) } /
             _ "address" _ { Expr::Address(None) }
 
         pub rule big_map() -> Expr =
-            _ "(big_map " _ label:label()? _ left:expr() _ right:expr() _ ")" {
+            _ "(big_map " _ label:label()? _ left:expr() _ right:expr() _ ")" _ {
                 Expr::BigMap(label, Box::new(left), Box::new(right))
             }
 
@@ -47,18 +47,18 @@ peg::parser! {
         pub rule label() -> std::string::String = "%" s:$(['a'..='z' | 'A'..='Z' | '_']+) {
             s.to_owned() }
 
-        pub rule map() -> Expr =
-            _ "(map " _ label:label()? _ left:expr() _ right:expr() _ ")" {
+        pub rule map() -> Expr =ma
+            _ "(map " _ label:label()? _ left:expr() _ right:expr() _ ")" _ {
                 Expr::Map(label, Box::new(left), Box::new(right))
             }
 
         pub rule nat() -> Expr = _ "(nat" _ l:label() _ ")" { Expr::Nat(Some(l)) } /
             _ "nat" _ { Expr::Nat(None) }
 
-        pub rule option() -> Expr = _ "(option" _ l:label() _ e:expr() _ ")" {
+        pub rule option() -> Expr = _ "(option" _ l:label() _ e:expr() _ ")" _ {
             Expr::Option_(Some(l), Box::new(e)) }
 
-        pub rule or() -> Expr = "(or" _ l:label()? _ left:expr() _ right:expr() ")"
+        pub rule or() -> Expr = "(or" _ l:label()? _ left:expr() _ right:expr() ")" _
             { Expr::Or(l, Box::new(left), Box::new(right)) }
 
         pub rule pair() -> Expr =
@@ -67,15 +67,15 @@ peg::parser! {
             }
 
         pub rule string() -> Expr =
-            _ "(string" _ l:label()  _ ")" { Expr::String(Some(l)) } /
+            _ "(string" _ l:label()  _ ")" _ { Expr::String(Some(l)) } /
             _ "string" _ { Expr::String(None) }
 
 
         pub rule timestamp() -> Expr =
-            _ "(timestamp" _ l:label() _ ")" { Expr::Timestamp(Some(l)) } /
+            _ "(timestamp" _ l:label() _ ")" _ { Expr::Timestamp(Some(l)) } /
             _ "timestamp" _ { Expr::Timestamp(None) }
 
-        pub rule unit() -> Expr = _ "(unit" _ l:label() _ ")" { Expr::Unit(Some(l)) } /
+        pub rule unit() -> Expr = _ "(unit" _ l:label() _ ")" _ { Expr::Unit(Some(l)) } /
             _ "unit" _ { Expr::Unit(None) }
 
     }
