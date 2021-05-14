5430a886dc67faf565ce815cd40b88fe4f9c3dbc
multi prise <korokoko.toi@gmail.com>
2021-05-14 13:53:33 +0200

    add opportunities to select which database to use for the scripts

--- test-pm2.sh
+++ test-pm2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql -c 'drop database tezos'
-psql -c 'create database tezos'
+psql $DATABASE_URL -c 'drop database tezos'
+psql $DATABASE_URL -c 'create database tezos'
 cargo run -- -c KT1LYbgNsG2GYMfChaVCXunjECqY59UJRWBf generate-sql|psql tezos
 cargo run -- -c KT1LYbgNsG2GYMfChaVCXunjECqY59UJRWBf -l 147816,147814,147813,147812,147811,147810,147809,147808,147807,147806,147805
