57379725545db6dcf644ca5edb73521741378cee
Rick Klomp <rick.klomp@tzconnect.com>
2021-09-09 12:52:27 +0200

    remove docker-compose and makefile (their setups were old, unmaintained)

--- env.example.sh
+++ env.example.sh
@@ -3,6 +3,7 @@
 NODE_URL=http://granada.newby.org:8732
 DATABASE_NAME=f1
 DATABASE_URL=postgres://newby:foobar@localhost/$DATABASE_NAME
-CONTRACT_ID=KT1XBbLxJBuxmsfmnTNarxfVjxaBnkTAtqn4
+CONTRACTS="f1=KT1XBbLxJBuxmsfmnTNarxfVjxaBnkTAtqn4"
+BCD_URL=https://api.better-call.dev/v1
+NETWORK=granadanet
 TIMESTAMP=`date +%s`
-
