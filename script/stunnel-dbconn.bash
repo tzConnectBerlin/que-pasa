2a22209d13d713150aaec0c1b422d2e57ca10e0a
Rick Klomp <rick.klomp@tzconnect.com>
2022-02-21 15:22:03 +0100

    add script for starting a ssl proxy (w/ stunnel, to replace lost --with-ssl)

---
+++ script/stunnel-dbconn.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+cd $(git rev-parse --show-toplevel)/script
+
+command -v stunnel >/dev/null 2>&1 || {
+    echo >&2 'stunnel is a required dependency, it is currently not installed.'
+    exit 1
+}
+
+[ -z $PGPORT ] && export PGPORT=5432
+
+[ -z $PGPORT_REMOTE ] && export PGPORT_REMOTE=5432
+[ -z $PGHOST_REMOTE ] && {
+    echo 'expected defined PGHOST_REMOTE env var'
+    exit 1
+}
+
+
+conf_file=`mktemp`
+echo "
+foreground = yes
+
+[pgcli]
+client  = yes
+protocol = pgsql
+accept  = localhost:$PGPORT
+connect = $PGHOST_REMOTE:$PGPORT_REMOTE
+debug   = 3
+options = NO_TICKET
+" > $conf_file
+
+stunnel $conf_file "$@"
