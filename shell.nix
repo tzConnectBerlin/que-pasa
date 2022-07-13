d5f384efd8023823e1ee8fb3139356555dfea70e
Rick Klomp <rick93nl@gmail.com>
2022-07-13 21:41:43 +0200

    add dependencies setup for vim rust LSP in shell.nix

--- shell.nix
+++ shell.nix
@@ -9,5 +9,29 @@ pkgs.mkShell {
     rust-bin.stable.latest.default
     pkg-config
     openssl
+
+    # this nix-shell additionally sets up dependencies for vim rust LSP
+    rust-analyzer
+    nodejs-18_x
   ];
+
+  shellHook =
+    ''
+      mkdir -p .vim
+
+      coc_settings_target=.vim/coc-settings.json
+      [[ ! -f $coc_settings_target || -L $coc_settings_target ]] || {
+        echo "warn: cannot setup rust-analyzer path for vim coc, $coc_settings_target is not a symlink and may contain information that should not be overwritten" >&2
+        return
+      }
+
+      coc="`mktemp`"
+      cat <<EOF > "$coc"
+      {
+         "rust-analyzer.server.path": "`which rust-analyzer`"
+      }
+      EOF
+
+      ln -s "$coc" $coc_settings_target
+    '';
 }
